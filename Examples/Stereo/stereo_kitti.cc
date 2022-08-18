98ede9225a04a40a10c2247089d6bcecc2dae59e
ubuntu <yikomuzi@gmail.com>
2022-08-18 08:32:52 +0000

    start

--- Examples/Stereo/stereo_kitti.cc
+++ Examples/Stereo/stereo_kitti.cc
@@ -42,7 +42,6 @@ int main(int argc, char **argv) {
     argv[2] = "/home/ubuntu/Desktop/ORB_SLAM2_study/ORB_SLAM2/Examples/Stereo/KITTI00-02.yaml";
     argv[3] = "/remote-home/2132917/kitti_datasets/dataset/sequences/00";
 
-
     if (argc != 4) {
         cerr << endl
              << "Usage: ./stereo_kitti path_to_vocabulary path_to_settings path_to_sequence" << endl;
@@ -64,12 +63,6 @@ int main(int argc, char **argv) {
     vector<float> vTimesTrack;
     vTimesTrack.resize(nImages);
 
-//    cout << endl
-//         << "-------" << endl;
-//    cout << "Start processing sequence ..." << endl;
-//    cout << "Images in the sequence: " << nImages << endl
-//         << endl;
-
     // Main loop
     cv::Mat imLeft, imRight;
     for (int ni = 0; ni < nImages; ni++) {
@@ -82,33 +75,9 @@ int main(int argc, char **argv) {
         imRight = cv::imread(vstrImageRight[ni], CV_LOAD_IMAGE_UNCHANGED);
         double tframe = vTimestamps[ni];
 
-//        if (imLeft.empty()) {
-//            cerr << endl
-//                 << "Failed to load image at: "
-//                 << string(vstrImageLeft[ni]) << endl;
-//            return 1;
-//        }
-
-//        std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
-
         // Pass the images to the SLAM system
         SLAM.TrackStereo(imLeft, imRight, tframe);
 
-//        std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
-
-//        double ttrack = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1).count();
-
-//        vTimesTrack[ni] = ttrack;
-
-//        // Wait to load the next frame
-//        double T = 0;
-//        if (ni < nImages - 1)
-//            T = vTimestamps[ni + 1] - tframe;
-//        else if (ni > 0)
-//            T = tframe - vTimestamps[ni - 1];
-//
-//        if (ttrack < T)
-//            usleep((T - ttrack) * 1e6);
         usleep(1000 * 1000 * 25);
 
     }
--- src/Tracking.cc
+++ src/Tracking.cc
@@ -282,10 +282,6 @@ namespace ORB_SLAM2 {
         if (mState == NOT_INITIALIZED) {
             StereoInitialization();                                                     // RGBD第一帧初始化
 
-
-
-
-
             mpFrameDrawer->Update(this);
 
             if (mState != OK)
